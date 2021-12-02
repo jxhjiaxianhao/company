f34d89f9003753fe332788e7aeaf1109a169e84c
jxh <Cus123456~>
2021-12-02 16:38:48 +0800

    Revert "第10次"
    This reverts commit b2da9c6cfd3cc00d9b57cc372c3be9c647d8336a.

--- src/main/java/com/example/test/cn/jiaxianhao/Hello.java
+++ src/main/java/com/example/test/cn/jiaxianhao/Hello.java
@@ -1,8 +1,4 @@
 package com.example.test.cn.jiaxianhao;
 
 public class Hello {
-    @Override
-    protected Object clone() throws CloneNotSupportedException {
-        return super.clone();
-    }
 }
