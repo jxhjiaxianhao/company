5d39d8d5a3de5327c4c92e12ff489efa1f272f34
jiaxianhao <1605973013@qq.com>
2022-06-24 09:47:00 +0800

    333333333

--- src/main/java/com/example/test/cn/User.java
+++ src/main/java/com/example/test/cn/User.java
@@ -1,17 +1,29 @@
 package com.example.test.cn;
 
+import com.example.test.cn.jiaxianhao.CardDto;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
+import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.Mapping;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
+@Repository
 @Mapper
-public interface User {
-    @Select("SELECT name  FROM user where id =#{id}")
+public interface User  {
+    @Select("SELECT *  FROM user where id =#{id}")
     List<Map<String,String>> getUser(Integer id);
+
+    @Select("SELECT *  FROM user where createtime =#{id}")
+    List<Map<String,String>> getUser1(Date id);
+
+    @Select("select * from card where id =#{id}")
+    List<Map> se(@Param("id") int id);
+
 
 
 
