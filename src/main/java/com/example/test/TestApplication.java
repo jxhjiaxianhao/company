cf4fa7a38dbf0bc1eebb1d4c42e51de1add2d327
jiaxianhao <1605973013@qq.com>
2022-06-24 12:48:58 +0800

    333333333

--- src/main/java/com/example/test/TestApplication.java
+++ src/main/java/com/example/test/TestApplication.java
@@ -1,10 +1,16 @@
 package com.example.test;
 
+import com.example.test.cn.impl.Do;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+
+import java.util.function.Function;
 
 @SpringBootApplication
+//@MapperScan("com.example.*")
 public class TestApplication {
 
     public static void main(String[] args) {
@@ -12,6 +18,17 @@ public class TestApplication {
         Object son = run.getBean("Son");
         System.out.println(son);
 
+    }
+
+    @Bean
+    public Function<String, String> uppercase() {
+        return value ->
+                value.toUpperCase();
+    }
+
+    @Bean
+    public Do uppercase1() {
+        return () -> System.out.println("000");
     }
 
 }
